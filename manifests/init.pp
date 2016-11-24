0aef99d4ae47e1ed9cdb0eedcfc8ca84881822b5
Adria Bisbe <adria.bisbe@gmail.com>
2016-11-24 19:40:04 +0100

    network stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,12 @@
 class standalone (
   $base_path,
+  $root_password,
+  $users,
+  $network_port_knock_ports = undef,
+  $network_allowed_networks = undef,
+  $cosmetic_ps1 = '[\[\033[01;32m\]\u\[\033[00m\]@\h \[\033[01;34m\]\W\[\033[00m\]]\$ ',
+  $cosmetic_ps1_root = '[\[\033[01;31m\]\u\[\033[00m\]@\h \[\033[01;34m\]\W\[\033[00m\]]\$ ',
+
 ){
 
   # Useful script to force a puppet run at any time
@@ -8,5 +15,59 @@ class standalone (
     content => template("${module_name}/repuppet.erb"),
   }
 
+  # User config
+  class { '::root':
+    password => $root_password,
+  }
 
+  $users.each |String $group, Hash $g_users| {
+    group { $group: ensure => present}
+
+    $g_users.each |String $user, Hash $u_data| {
+      user { $user:
+        groups   => $group,
+        home     => "/home/${user}",
+        shell    => '/bin/bash',
+        password => $u_data['password'],
+        purge_ssh_keys => true,
+        managehome => true,
+      }
+
+      ssh_authorized_key { $u_data['email']:
+        ensure => present,
+        user   => $user,
+        type   => 'ssh-rsa',
+        key    => $u_data['ssh_key'],
+      }
+
+    }
+  }
+
+
+  # Cosmetic configs
+  class { '::cosmetic::bash':
+    ps1      => $cosmetic_ps1,
+    ps1_root => $cosmetic_ps1_root,
+  }
+
+  include '::cosmetic::vimrc'
+
+  # Network
+
+  if ($network_port_knock_ports == undef or size($network_port_knock_ports) != 3) and $network_allowed_networks == undef {
+    fail('You should configure $network_port_knock_ports or $network_allowed_networks so you are able to access the server')
+  }
+
+  class { '::rhel::firewall':
+    src_ssh => $network_allowed_networks,
+  }
+
+  if $network_port_knock_ports != undef and size($network_port_knock_ports) == 3 {
+    rhel::firewall::portknock { 'SSH':
+      port1  => $network_port_knock_ports[0],
+      port2  => $network_port_knock_ports[1],
+      port3  => $network_port_knock_ports[2],
+      dports => [ '22' ],
+    }
+  }
 }
